96ccdf6865dc76b2976308ae8b90b7bf124bf25d
Natcardoso <nataliaalmeidacardoso2001@gmail.com>
2024-08-12 19:28:39 -0300

    adicionando a font e começando a tela de dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,8 +2,11 @@ import { Menu } from '../../components/Menu';
 
 export const Dashboard = () => {
   return (
-    <div className="flex h-full w-full border-solid border-2">
+    <div className="flex h-screen gap-2 w-screen border-solid border-2">
       <Menu />
+      <div className="bg-white rounded-lg flex-1 p-5">
+        <h1 className="font-light tracking-[1.5rem] text-5xl">DASHBOARD</h1>
+      </div>
     </div>
   );
 };
